b631e96c52fb5e46dfdc765d828a78e6d77dd0de
canito481813-dotcom <c.anito.481813@umindanao.edu.ph>
2026-02-25 09:49:46 +0800

    Add files via upload

---
+++ Anitophplesson/Auth/welcome.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+$session_id = session_id();
+if (isset($_SESSION['username'])) {
+    header("Location: LOGIN.php");
+    exit();
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Welcome Page</title>
+</head>
+<body>
+
+<h2>Welcome Page</h2>
+
+<p>
+    Hi <?php echo $_SESSION['username']; ?>
+</p>
+
+<a href = "logout.php"> Logout </p>
+
+</body>
+
+<script>
+    let phpSessionId + "<?php echo $session_id; ?>";
+    sessionStorage.setItem("PHP_SESSION_ID", phpSessionId);
+    console.log ("Session ID:", phpSessionId);
+</script>
+</html>
